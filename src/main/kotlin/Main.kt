c298405f89f8ec019b3e7e4fddd15cb3b2ad952c
theo47th <145131641+theo47th@users.noreply.github.com>
2024-12-07 22:59:57 +0000

    add description project was made but not put in git hub

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+package ie.setu
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
-}
+
+/*There will be the main menu for the app and also the
+* user interface that a fan of cars like myself would likely love
+* to have I am going to create data of cars with valid information,
+* and we are going to call out the data whilst using the interface
+* and also add new cars that the user really loves this would help grow .
+* the community with knowledge about new models hopefully with the rewards
+* system il have a way to store it for the user*/
